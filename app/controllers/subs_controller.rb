2d53ef88753686c752574741358b24e8e809dde1
Santiago Lozano Ventura <santiago.ventura2012@gmail.com>
2021-02-24 21:34:24 -0700

    first commit

---
+++ app/controllers/subs_controller.rb
@@ -0,0 +1,14 @@
+class SubsController < ApplicationController
+  def index
+    render component: "Subs"
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
---
+++ app/javascript/components/Subs.js
@@ -0,0 +1,12 @@
+// import React from 'core-js/library/fn/reflect/es7/get-metadata'
+import React from 'react'
+
+export default () => {
+
+  return (
+    <>
+      <h1>Shoppee</h1>
+    </>
+  );
+
+}
